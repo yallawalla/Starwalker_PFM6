5bcd333c33c5cf5787b21b14fc6471cb658078d8
toshiba <mr.mocnik@gmail.com>
2016-03-08 23:57:56 +0100

    rgb-hsv

--- Project/Pfm6Ctrl/MDK-ARM/xxx.c
+++ Project/Pfm6Ctrl/MDK-ARM/xxx.c
@@ -124,6 +124,20 @@ GPIO_InitTypeDef					GPIO_InitStructure;
 	*/
 /*******************************************************************************/
 void		trigger() {
+int			i,j,k;
+struct 	_dma	*p;
+	
+				for(i=0; rgb[i].size; ++i)
+					for(j=0; j<rgb[i].size; ++j)
+						if(rgb[i].color_buffer)
+							for(k=0,p=rgb[i].dma; k<8; ++k) {
+								(rgb[i].color_buffer[j].i & (0x000080>>k)) ? (p[j].b[k]=53)	: (p[j].b[k]=20);
+								(rgb[i].color_buffer[j].i & (0x008000>>k)) ? (p[j].g[k]=53)	: (p[j].g[k]=20);
+								(rgb[i].color_buffer[j].i & (0x800000>>k)) ? (p[j].r[k]=53)	: (p[j].r[k]=20);
+							}
+						else
+							for(k=0,p=rgb[i].dma; k<24; ++k)
+									p[j].g[k]=20;
 				
 				DMA_Cmd(DMA1_Stream1, DISABLE);
 				TIM_Cmd(TIM2,DISABLE);
@@ -174,8 +188,7 @@ void		trigger_TIM() {
 }
 //______________________________________________________________________________________
 void			*procLeds(void *v) {
-int				i,j,k;
-struct 		_dma	*p;
+int				i,j;
 	
 					for(i=0; rgb[i].dma; ++i)
 						switch(rgb[i].command) {
@@ -195,9 +208,9 @@ struct 		_dma	*p;
 									else if(rgb[i].color_buffer[j].c[1] < rgb[i].color.c[1]) 
 										++rgb[i].color_buffer[j].c[1];
 									else if(rgb[i].color_buffer[j].c[2] > rgb[i].color.c[2]) 
-										--rgb[i].color_buffer[j].c[3];
+										--rgb[i].color_buffer[j].c[2];
 									else if(rgb[i].color_buffer[j].c[2] < rgb[i].color.c[2]) {
-										++rgb[i].color_buffer[j].c[3];
+										++rgb[i].color_buffer[j].c[2];
 										break;
 										}
 									}
@@ -211,17 +224,6 @@ struct 		_dma	*p;
 							 break;
 						}
 
-					for(i=0; rgb[i].size; ++i)
-						for(j=0; j<rgb[i].size; ++j)
-							if(rgb[i].color_buffer)
-								for(k=0,p=rgb[i].dma; k<8; ++k) {
-									(rgb[i].color_buffer[j].i & (0x000080>>k)) ? (p[j].b[k]=53)	: (p[j].b[k]=20);
-									(rgb[i].color_buffer[j].i & (0x008000>>k)) ? (p[j].g[k]=53)	: (p[j].g[k]=20);
-									(rgb[i].color_buffer[j].i & (0x800000>>k)) ? (p[j].r[k]=53)	: (p[j].r[k]=20);
-								}
-							else
-								for(k=0,p=rgb[i].dma; k<24; ++k)
-									p[j].g[k]=20;
 					trigger();
 					return NULL;
 }
@@ -257,4 +259,130 @@ int				SetColor(char *c) {
 					}
 				}	
 				return _PARSE_OK;
+}
+//______________________________________________________________________________________
+struct RGB_set {
+ unsigned char r;
+ unsigned char g;
+ unsigned char b;
+} RGB_set;
+ 
+struct HSV_set {
+ signed int h;
+ unsigned char s;
+ unsigned char v;
+} HSV_set;
+
+/*******************************************************************************
+ * Function RGB2HSV
+ * Description: Converts an RGB color value into its equivalen in the HSV color space.
+ * Copyright 2010 by George Ruinelli
+ * The code I used as a source is from http://www.cs.rit.edu/~ncs/color/t_convert.html
+ * Parameters:
+ *   1. struct with RGB color (source)
+ *   2. pointer to struct HSV color (target)
+ * Notes:
+ *   - r, g, b values are from 0..255
+ *   - h = [0,360], s = [0,255], v = [0,255]
+ *   - NB: if s == 0, then h = 0 (undefined)
+ ******************************************************************************/
+void RGB2HSV(struct RGB_set RGB, struct HSV_set *HSV){
+ unsigned char min, max, delta;
+ 
+ if(RGB.r<RGB.g)min=RGB.r; else min=RGB.g;
+ if(RGB.b<min)min=RGB.b;
+ 
+ if(RGB.r>RGB.g)max=RGB.r; else max=RGB.g;
+ if(RGB.b>max)max=RGB.b;
+ 
+ HSV->v = max;                // v, 0..255
+ 
+ delta = max - min;                      // 0..255, < v
+ 
+ if( max != 0 )
+ HSV->s = (int)(delta)*255 / max;        // s, 0..255
+ else {
+ // r = g = b = 0        // s = 0, v is undefined
+ HSV->s = 0;
+ HSV->h = 0;
+ return;
+ }
+ 
+ if( RGB.r == max )
+ HSV->h = (RGB.g - RGB.b)*60/delta;        // between yellow & magenta
+ else if( RGB.g == max )
+ HSV->h = 120 + (RGB.b - RGB.r)*60/delta;    // between cyan & yellow
+ else
+ HSV->h = 240 + (RGB.r - RGB.g)*60/delta;    // between magenta & cyan
+ 
+ if( HSV->h < 0 )
+ HSV->h += 360;
+}
+/*******************************************************************************
+ * Function HSV2RGB
+ * Description: Converts an HSV color value into its equivalen in the RGB color space.
+ * Copyright 2010 by George Ruinelli
+ * The code I used as a source is from http://www.cs.rit.edu/~ncs/color/t_convert.html
+ * Parameters:
+ *   1. struct with HSV color (source)
+ *   2. pointer to struct RGB color (target)
+ * Notes:
+ *   - r, g, b values are from 0..255
+ *   - h = [0,360], s = [0,255], v = [0,255]
+ *   - NB: if s == 0, then h = 0 (undefined)
+ ******************************************************************************/
+void HSV2RGB(struct HSV_set HSV, struct RGB_set *RGB){
+ int i;
+ float f, p, q, t, h, s, v;
+ 
+ h=(float)HSV.h;
+ s=(float)HSV.s;
+ v=(float)HSV.v;
+ 
+ s /=255;
+ 
+ if( s == 0 ) { // achromatic (grey)
+ RGB->r = RGB->g = RGB->b = v;
+ return;
+ }
+ 
+ h /= 60;            // sector 0 to 5
+ i = floor( h );
+ f = h - i;            // factorial part of h
+ p = (unsigned char)(v * ( 1 - s ));
+ q = (unsigned char)(v * ( 1 - s * f ));
+ t = (unsigned char)(v * ( 1 - s * ( 1 - f ) ));
+ 
+ switch( i ) {
+ case 0:
+ RGB->r = v;
+ RGB->g = t;
+ RGB->b = p;
+ break;
+ case 1:
+ RGB->r = q;
+ RGB->g = v;
+ RGB->b = p;
+ break;
+ case 2:
+ RGB->r = p;
+ RGB->g = v;
+ RGB->b = t;
+ break;
+ case 3:
+ RGB->r = p;
+ RGB->g = q;
+ RGB->b = v;
+ break;
+ case 4:
+ RGB->r = t;
+ RGB->g = p;
+ RGB->b = v;
+ break;
+ default:        // case 5:
+ RGB->r = v;
+ RGB->g = p;
+ RGB->b = q;
+ break;
+ }
 }
